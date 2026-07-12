a9195fa88bea7362c2ab6cd9079c4711a8f9e0da
VH-AJEETHAAPARNA <v.ajeethaaparna@gmail.com>
2026-07-12 10:21:26 +0530

    Initial scaffolding for Maintenance, Fuel, Expenses, Reports modules

---
+++ transitops_fuel/models/fuel_log.py
@@ -0,0 +1,24 @@
+from odoo import models, fields
+
+
+class TransitopsFuelLog(models.Model):
+    _name = 'transitops.fuel.log'
+    _description = 'Vehicle Fuel Log'
+
+    vehicle_id = fields.Many2one(
+        'transitops.vehicle',
+        string='Vehicle',
+        required=True,
+    )
+    liters = fields.Float(
+        string='Liters',
+    )
+    cost = fields.Float(
+        string='Cost',
+    )
+    date = fields.Date(
+        string='Date',
+    )
+    distance = fields.Float(
+        string='Distance',
+    )
---
+++ transitops_reports/models/report.py
@@ -0,0 +1,8 @@
+from odoo import models
+
+
+class TransitopsReport(models.Model):
+    _name = 'transitops.report'
+    _description = 'Fleet KPI Placeholder'
+
+    # TODO: Add KPI fields (e.g. total_cost, avg_consumption, fleet_utilisation)
